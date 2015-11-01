ef693ffca8ad351ed6227d189673fffaeb77f9e8
Amy Williams <amywilliams@Amys-Air.lan>
2015-11-01 18:28:46 -0300

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Famy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.serve_static_assets = true
-    config.assets.compile = true
+    #config.serve_static_assets = true
+    #config.assets.compile = true
   end
 end
